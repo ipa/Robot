634dc9f6d943758fa9cc6c6698ddd5e2c18bd599
Iwan Paolucci <iwan.paolucci@stud.hslu.ch>
2011-12-14 00:10:34 +0100

    added protocoldefinition for drivecommands

---
+++ RobotDriveProtocol/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotDriveProtocol
+{
+    public class Command
+    {
+        public string Method;
+
+        public Dictionary<Type, Object> Parameters;
+
+        public override string ToString()
+        {
+            string args = "";
+            this.Parameters.Values.ToList().ForEach(el => args += (el.ToString() + " : "));
+            return string.Format("{0} - {1}", this.Method, args);
+        }
+    }
+}
