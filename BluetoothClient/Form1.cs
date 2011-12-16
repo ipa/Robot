fa5197f46f0b4882094da0b90fa714076dc4065a
Iwan Paolucci <iwan.paolucci@stud.hslu.ch>
2011-12-16 15:18:35 +0100

    fertig

--- BluetoothClient/Form1.cs
+++ BluetoothClient/Form1.cs
@@ -21,6 +21,7 @@ namespace BluetoothClient
 
         private void btnDeleteCommand_Click(object sender, EventArgs e)
         {
+            
             lstCommands.Items.Remove(lstCommands.SelectedItem);
         }
 
@@ -32,14 +33,14 @@ namespace BluetoothClient
             {
 
                 //RunLine(float length, float speed, float acceleration)
-                
+
                 cmd.Method = "RunLine";
                 cmd.Parameters.Add(float.Parse(txtTrackLineLength.Text));
 
                 cmd.Parameters.Add(1.0f);
-                
+
                 cmd.Parameters.Add(1.0f);
-            
+
             }
             else if (tabControl.SelectedTab == tpgTrackTurn)
             {
@@ -50,12 +51,12 @@ namespace BluetoothClient
                 cmd.Parameters.Add(1.0f);
 
                 cmd.Parameters.Add(1.0f);
-            
+
             }
             else if (tabControl.SelectedTab == tpgTrackArc)
             {
                 //     public void RunArcLeft(float radius, float angle, float speed, float acceleration)
-       
+
                 if (rbnTrackArcLeft.Checked)
                 {
                     cmd.Method = "RunArcLeft";
@@ -80,41 +81,38 @@ namespace BluetoothClient
 
         }
 
-        
+
         private void button1_Click(object sender, EventArgs e)
-        {try
-                {
-            Guid service = BluetoothServiceList.Robot09;
-            List<Command> elCommandos = lstCommands.Items.OfType<Command>().ToList();
-
-            TA.Bluetooth.BluetoothClient bc = BluetoothDevice.Connect(((RobotItem)cbxRobot.SelectedItem).mac, service);
-
-            if (bc != null && bc.IsConnected())
+        {
+            try
             {
-                 
-                  
+                Guid service = BluetoothServiceList.Robot06;
+                List<Command> elCommandos = lstCommands.Items.OfType<Command>().ToList();
 
+                TA.Bluetooth.BluetoothClient bc = BluetoothDevice.Connect(((RobotItem)cbxRobot.SelectedItem).mac, service);
+
+                if (bc != null)
+                {
                     // read transmitted data
                     
                     StreamWriter sw = new StreamWriter(bc.GetStream());
                     // request
-                    
+
                     sw.WriteLine(CommandSerializer.Serialize(elCommandos));
                     sw.Flush();
                     // print 
-                    bc.Close();
+                    sw.Close();
                     MessageBox.Show("Data sent!");
-               
-            }
-            else
-            {
-                MessageBox.Show("No Device found");
-            }
                 }
-        catch (Exception ex)
-        {
-            MessageBox.Show(ex.ToString());
-        }
+                else
+                {
+                    MessageBox.Show("No Device found");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         public class RobotItem
@@ -134,31 +132,31 @@ namespace BluetoothClient
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-             cbxRobot.Items.Add(new RobotItem("HSLU_01", "001BDC002157"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_02", "001BDC0FFDEF"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_03", "001BDC0021C4"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_04", "001BDC0F8F39"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_05", "001BDC00202F"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_06", "001BDC0003CC"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_07", "001BDC002140"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_08", "001BDC0FFEDE"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_09", "001BDC0FFE1C"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_10", "001BDC000023"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_11", "001BDC0021F1"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_12", "001BDC002145"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_13", "001BDC00034C"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_14", "001BDC002139"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_15", "001BDC0FF453"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_16", "001BDC0022D5"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_17", "001BDC0022D6"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_18", "001BDC00212A"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_19", "001BDC0FFDED"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_20", "001BDC0FF360"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_21", "001BDC0FE81D"));
-             cbxRobot.Items.Add(new RobotItem("HSLU_25", ""));
-             cbxRobot.Items.Add(new RobotItem("HSLU_30", "001BDC0021D1"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_01", "001BDC002157"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_02", "001BDC0FFDEF"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_03", "001BDC0021C4"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_04", "001BDC0F8F39"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_05", "001BDC00202F"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_06", "001BDC0003CC"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_07", "001BDC002140"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_08", "001BDC0FFEDE"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_09", "001BDC0FFE1C"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_10", "001BDC000023"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_11", "001BDC0021F1"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_12", "001BDC002145"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_13", "001BDC00034C"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_14", "001BDC002139"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_15", "001BDC0FF453"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_16", "001BDC0022D5"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_17", "001BDC0022D6"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_18", "001BDC00212A"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_19", "001BDC0FFDED"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_20", "001BDC0FF360"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_21", "001BDC0FE81D"));
+            cbxRobot.Items.Add(new RobotItem("HSLU_25", ""));
+            cbxRobot.Items.Add(new RobotItem("HSLU_30", "001BDC0021D1"));
 
-            
+
         }
     }
 }
